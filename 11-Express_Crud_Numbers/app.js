e160455db31545962dab3fbc27694ce7e2659ff4
Yitz <chalfari@gmail.com>
2022-01-12 13:23:52 +0200

    add error handling to exercise 11

--- 11-Express_Crud_Numbers/app.js
+++ 11-Express_Crud_Numbers/app.js
@@ -2,24 +2,43 @@ const express = require("express");
 const numbers = [1, 2, 3, 4, 5];
 const app = express();
 const PORT = 3000;
-
+app.use(express.json());
+app.use(express.text());
 app.get("/numbers", (req, res) => {
   res.send(numbers);
 });
 app.post("/numbers", (req, res) => {
-  numbers.push(req.body);
-  res.send(numbers);
+  if (numbers.includes(parseInt(req.body.num))) {
+    res.status(400).send("Number already exists");
+  } else {
+    numbers.push(parseInt(req.body.num));
+    res.send(numbers);
+  }
 });
 app.delete("/numbers/:num", (req, res) => {
   const index = numbers.indexOf(parseInt(req.params.num));
-  console.log(index);
-  numbers.splice(index, 1);
-  res.send(numbers);
+  if (index === -1) {
+    res.status(404).send("Not Found");
+  } else {
+    numbers.splice(index, 1);
+    res.send(numbers);
+  }
 });
-app.put("/numbers/:num/:replacement", (req, res) => {
+app.put("/numbers/:num", (req, res) => {
   const index = numbers.indexOf(parseInt(req.params.num));
-  numbers.splice(index, 1, parseInt(req.params.replacement));
-  res.send(numbers);
+  if (index !== -1 && req.body.num) {
+    numbers.splice(index, 1, parseInt(req.body.num));
+    res.send(numbers);
+  } else {
+    res.send("Please enter an existing number and a replacement");
+  }
 });
+//!  using 2 params
+// app.put("/numbers/:num/:replacement", (req, res) => {
+//   const index = numbers.indexOf(parseInt(req.params.num));
+//   numbers.splice(index, 1, parseInt(req.params.replacement));
+//   res.send(numbers);
+// });
+//! can also use queries or just add another key value to the req.body
 
 app.listen(PORT, () => console.log("listening"));
